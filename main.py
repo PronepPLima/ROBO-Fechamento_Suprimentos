66fd0714d8d0d211326de94b73470cdfcc50aec0
PronepPLima <pietro.lima@pronep.com.br>
2024-04-10 09:36:14 -0300

    Inserção de logo pronep e seu redimencionamento

--- main.py
+++ main.py
@@ -3,7 +3,14 @@
 
 
 import tkinter as tk
+from PIL import Image, ImageTk
 
+#carregando imagem original:
+imagem_original = Image.open("LOGO_PRETA.png")
+largura=300
+altura=140
+#redimencionando a imagem:
+imagem_redimencionada = imagem_original.resize((largura,altura))
 
 if __name__ == "__main__":    
     print("\n============================== inicio ========================")
@@ -15,8 +22,14 @@ if __name__ == "__main__":
     root.title("ROBO - Fechamento Suprimentos 1.0")
     root.configure(bg="white")
     
-
+    #inserir imagem de logo pronep mas redimencionada
+    #imagem = tk.PhotoImage(file="LOGO_PRETA.png")
+    #lb_barra_superior = tk.Label(root, image=imagem , border =0 , height=148 , width=320)
+    #lb_barra_superior.pack()
     
+    imagem_tk = ImageTk.PhotoImage(imagem_redimencionada)
+    lb_barra_superior = tk.Label(root, image=imagem_tk , border =0)
+    lb_barra_superior.pack()
     
     root.mainloop()
     
