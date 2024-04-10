746e57c0aedef1d2f5afe888cb2832f4afcbef42
PronepPLima <pietro.lima@pronep.com.br>
2024-04-10 09:21:49 -0300

    Criação de projeto + adição de Interface gráfica

---
+++ main.py
@@ -0,0 +1,31 @@
+#10/04/2023
+#@PLima
+
+
+import tkinter as tk
+
+
+if __name__ == "__main__":    
+    print("\n============================== inicio ========================")
+    
+    #montar interface gráfica:
+    root = tk.Tk()
+    root.maxsize(800,600)
+    root.geometry("800x600")
+    root.title("ROBO - Fechamento Suprimentos 1.0")
+    root.configure(bg="white")
+    
+
+    
+    
+    root.mainloop()
+    
+    #abrir pasta arquivos, que fica na pasta da instalacao do app
+    
+    #to do: criar interface gráfica com botão de chose file
+    
+    #abrir planilha csv com fechamento do mes selecionado
+    
+    
+    
+    print("\n============================== fim ========================")
