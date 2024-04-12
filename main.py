5f0d4b75e5b2d044165f0aa7f9f51c7d4063f39d
PronepPLima <pietro.lima@pronep.com.br>
2024-04-12 17:10:48 -0300

    Exibindo em console e Label, data frame importado a partir da 1 linha e determinadas colunas

--- main.py
+++ main.py
@@ -5,10 +5,17 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog
-import pdfplumber
 
-#from PyPDF2 import PdfReader
+import pandas as pd
+import numpy as py
 
+import warnings
+
+
+from pandastable import Table
+
+#ignorando alertas exibidos:
+warnings.filterwarnings("ignore")
 
 #carregando imagem original:
 imagem_original = Image.open("LOGO_PRETA.png")
@@ -17,26 +24,30 @@ altura=140
 #redimencionando a imagem:
 imagem_redimencionada = imagem_original.resize((largura,altura))
 
+    
+
 def UploadAction(event=None):
     filename = filedialog.askopenfilename()
-    print(f"Endereco do arquivo: {filename}")
+    print(f"Endereco do arquivo: {filename}\n")
+    
     #apos o click, exibicao do endereco do arquivo em console:
     label_file_path.config(text=filename)
+    label_file_path.pack(pady=10)
     
-    #abrir pdf com fechamento do mes selecionado  
-    with pdfplumber.open(filename) as pdf:
-        print(f"\nPaginas: {pdf.pages}\n")
-        num_paginas = len(pdf.pages)
-        print(f"\nNumero de paginas: {num_paginas}")
-        
-        #TO DO: ler cada página e exibir no console:       
-        primeira_pagina = pdf.pages[0]
-        texto = primeira_pagina.extract_text()
-    #print(f'Texto: {texto}')
+    #B) Abrir planilha xlsx e exibir em tela:
     
-    #exibir pdf com fechamento do mes selecionado   
-    label_file_dados.config(text=f'{texto}')
+    #TO DO: refatorar linha abaixo, pois ela se trata exclusivamente de teste:
+    data_frame = pd.read_excel('C:/Pietro/Projetos/ROBO-Fechamento_Suprimentos/arquivos/Fechamento Estoque v2 - para export 01012024 ate 31012024 000.xlsx',index_col=None)
+    
+    #TO DO: linha abaixo importa o data frame corretamente:
+    #data_frame = pd.read_excel(filename)
+    
+    data_frame.columns = data_frame.iloc[0]
+    print(data_frame[[ 'COD_MATERIAL', 'NOME_MATERIAL','VALOR_FINAL']].iloc[1:99999999])
 
+    label_file_dados.config(text=data_frame[[ 'COD_MATERIAL', 'NOME_MATERIAL','VALOR_FINAL', 'VALOR_E_TOTAL' , 'VALOR_S_TOTAL']].iloc[1:99999999])
+    label_file_dados.pack(pady=10)
+    
 
 #============================================================ EXECUCAO ============================================================
 if __name__ == "__main__":
@@ -45,8 +56,9 @@ if __name__ == "__main__":
 
         #montar interface gráfica:
         root = tk.Tk()
-        root.maxsize(800,600)
-        root.geometry("800x600")
+        #root.maxsize(800,600)
+        root.geometry("1024x800")
+        root.maxsize(1024,1024)
         root.title("ROBO - Fechamento Suprimentos 1.0")
         root.configure(bg="white")
 
@@ -60,11 +72,20 @@ if __name__ == "__main__":
 
         #exibindo em tela o endereco do arquivo selecionado no botao chosefile:
         label_file_path = tk.Label(root,text='',border =0)
-        label_file_path.pack(pady=10)
+        
+        #exibindo o número de páginas do pdf:
+        #label_file_num_pag = tk.Label(root,text='',border =0)
+        
 
         #TO DO: exibindo em tela pdf com fechamento do mes selecionado:
         label_file_dados = tk.Label(root,text='',border =0)
-        label_file_dados.pack(pady=10)
+        
+        """
+        text_widget = tk.Text(root)
+        
+        frame = tk.Frame(root)
+        frame.pack()
+        """
 
         root.mainloop()
         print("\n============================== fim ========================")
